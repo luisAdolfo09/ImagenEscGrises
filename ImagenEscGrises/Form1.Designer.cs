fa2b260fbe806c3775e0b76f1e519a09d3d1f239
jose <joreal06@gmail.com>
2024-04-21 23:27:28 -0600

    detalles con el loading

--- ImagenEscGrises/Form1.Designer.cs
+++ ImagenEscGrises/Form1.Designer.cs
@@ -66,6 +66,7 @@
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
--- ImagenEscGrises/Form1.cs
+++ ImagenEscGrises/Form1.cs
@@ -5,7 +5,7 @@ namespace ImagenEscGrises
 {
     public partial class Form1 : Form
     {
- 
+
         private List<Bitmap> imagenes_grises = new List<Bitmap>();
         private Bitmap copia;
         public Form1()
@@ -16,6 +16,7 @@ namespace ImagenEscGrises
 
         private async void btn1_Click(object sender, EventArgs e)
         {
+            pictureBox1.Visible = true;
             using (OpenFileDialog o = new OpenFileDialog())
             {
                 o.Filter = "Archivos de tipo JPG (*.jpg)|*.jpg|PNG (*.png)|*.png|Todos los archivos (*.*)|*.*";
@@ -32,10 +33,10 @@ namespace ImagenEscGrises
                         {
                             copia = new Bitmap(rutas);
 
-                            Task<Bitmap> gris = Task.Run(() => 
+                            Task<Bitmap> gris = Task.Run(() =>
                             {
-                                Imagen_Gris im = new Imagen_Gris(); return im.Imagen_final(copia); 
-                             });
+                                Imagen_Gris im = new Imagen_Gris(); return im.Imagen_final(copia);
+                            });
 
                             imagenes_grises.Add(await gris);
 
@@ -44,19 +45,19 @@ namespace ImagenEscGrises
                         string carpetaEscritorio = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
 
-                     
+
                         for (int i = 0; i < imagenes_grises.Count; i++)
                         {
-                            string nombreArchivo = $"imagen_{i}.png"; 
+                            string nombreArchivo = $"imagen_{i}.png";
                             string rutaCompleta = Path.Combine(carpetaEscritorio, nombreArchivo);
 
-                            
+
                             imagenes_grises[i].Save(rutaCompleta, ImageFormat.Png);
                         }
 
                         MessageBox.Show($"se guardaron las imagenes en {carpetaEscritorio}");
 
-
+                        pictureBox1.Visible = false;
                     }
                     catch (Exception ex)
                     {
@@ -67,5 +68,9 @@ namespace ImagenEscGrises
             }
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+        }
     }
 }
